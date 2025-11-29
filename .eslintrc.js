f5a260ec81783f0671492fabd4919dfe3d8dd3bb
genzwildsoul0824 <promotion0824@gmail.com>
2025-11-29 04:42:45 -0800

    fix: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        requireConfigFile: false,
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+  ],
 };
